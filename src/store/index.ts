5c427b61ea252630f2d9dc5b8f05206621a501f1
czj <11>
2021-02-05 16:29:41 +0800

    登录所需信息

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,31 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    token:localStorage.getItem("token"),
+    userInfo:JSON.parse(sessionStorage.getItem("userInfo") || "")
+  },
+  mutations: {
+    SET_TOKEN:(state,token)=>{
+      state.token=token;
+      localStorage.setItem("token",token);
+    },
+    SET_USERINFO:(state,userInfo)=>{
+      state.userInfo=userInfo;
+      sessionStorage.setItem("userInfo",JSON.stringify(userInfo));
+    },
+    REMOVE_INFO:(state)=>{
+      state.token="";
+      state.userInfo={};
+      localStorage.setItem("token","");
+      sessionStorage.setItem("userInfo",JSON.stringify(''));
+    }
+  },
+  getters:{
+    userInfo:state=>{
+      return state.userInfo;
+    }
+  },
   actions: {},
   modules: {}
 });

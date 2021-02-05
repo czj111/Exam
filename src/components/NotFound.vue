bf8a8ee661bb528ff98901dd848a7bb783c09c75
czj <11>
2021-02-05 16:28:26 +0800

    404页面

---
+++ src/components/NotFound.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="error-block">
+          <img v-bind:src="img"/>
+          <h3>{{error}}</h3>
+        </div>  
+    </div>
+</template>
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  name:"notFound",
+  data(){
+    return {
+      error:"您要找的页面不存在",
+      img:require("../assets/404.png")
+    }
+  }
+})
+</script>
+<style scoped>
+.error-block {
+    padding-top: 80px;
+    padding-bottom: 20px;
+    text-align: center;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import axios from 'axios';
+//引入element-ui
+import Element from 'element-ui';
+import "element-ui/lib/theme-chalk/index.css"
+Vue.use(Element);
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios=axios;
+router.beforeEach((to, from, next) =>{
+  console.log(to)
+    if(to.matched.length===0){
+      next('/404');
+    }else{
+      next();
+    }
+})
 
 new Vue({
   router,
